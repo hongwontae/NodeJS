a6431bb99a7f5a7b553728a54a67bea0aa0af329
dnjsxoghd <feelchok1234@gmail.com>
2023-11-29 11:02:24 +0900

    res Obj

--- NodeJSUdemy/NodeJSCode/Node-3-BasicConceptUnderstanding/firstNodeServer/app.js
+++ NodeJSUdemy/NodeJSCode/Node-3-BasicConceptUnderstanding/firstNodeServer/app.js
@@ -2,6 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers)
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>My First page</title></head>')
+    res.write('<body><h1>Hello Node.js World</h1></body>')
+    res.write('</html>')
 });
 
 server.listen(3000);
