8f5b804f24e0378e828e1b80ae686a5c4cd4e6e7
dnjsxoghd <feelchok1234@gmail.com>
2023-11-29 11:14:43 +0900

    Router request

--- NodeJSUdemy/NodeJSCode/Node-3-BasicConceptUnderstanding/firstNodeServer/app.js
+++ NodeJSUdemy/NodeJSCode/Node-3-BasicConceptUnderstanding/firstNodeServer/app.js
@@ -1,15 +1,24 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method, req.headers)
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<head><title>My First page</title></head>')
-    res.write('<body><h1>Hello Node.js World</h1></body>')
-    res.write('</html>')
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First page</title></head>");
+  res.write("<body><h1>Hello Node.js World</h1></body>");
+  res.write("</html>");
 });
 
 server.listen(3000);
-
 
 // 서버를 생성할 떄 사용하는 메서드이다
