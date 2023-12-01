9cdcc112baddd2f80172d240c124a8843da50fe8
dnjsxoghd <feelchok1234@gmail.com>
2023-12-01 15:51:54 +0900

    debugging +

--- NodeJSUdemy/NodeJSCode/Node-4-WorkflowAndDebugging/01-understanding-npm-scripts/routes.js
+++ NodeJSUdemy/NodeJSCode/Node-4-WorkflowAndDebugging/01-understanding-npm-scripts/routes.js
@@ -20,7 +20,7 @@ const requestHandler = (req, res) => {
     });
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[0];
+      const message = parsedBody.split('=')[1];
       fs.writeFile('message.txt', message, err => {
         res.statusCode = 302;
         res.setHeader('Location', '/');
