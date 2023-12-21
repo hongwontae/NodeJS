d2a649ad6e681ba8b1b1a5b414eb82c2990a75e6
dnjsxoghd <feelchok1234@gmail.com>
2023-12-21 11:38:06 +0900

    Sequelize Settings

--- NodeJSUdemy/NodeJSCode/Node-11-Sequelize/01-defining-a-model/util/database.js
+++ NodeJSUdemy/NodeJSCode/Node-11-Sequelize/01-defining-a-model/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', '1234', {
   dialect: 'mysql',
   host: 'localhost'
 });
